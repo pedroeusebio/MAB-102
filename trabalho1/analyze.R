5d67819b5a9031da0f9df8aa48ecefe37253696d
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-09-04 18:59:56 -0300

    Changging implementation of getStatType, now working with dataframe;

--- trabalho1/analyze.R
+++ trabalho1/analyze.R
@@ -25,23 +25,34 @@ getSizeType <- function(types, data) {
 }
 
 getStatsType <- function (types.1, types.2, data) {
-  size <- length(data$Name)
-  lst <- list()
+  nameVect <- c()
+  meanVect <- c()
+  medianVect <- c()
+  modeVect <- c()
+  maxVect <- c()
+  minVect <- c()
+  lengthVect <- c()
+  sizeVect <- c()
+  size <- 0
   for(t1 in types.1) {
     for(t2 in types.2) {
       type <- paste(t1, t2, sep='-')
-      element.1 <- data$Type.1 %in% t1 
-      element.2 <- data$Type.2 %in% t2
-      element <- element.1 & element.2
-      element <- data$HP[element]
+      element <- data$HP[data$Type.1 %in% t1 & data$Type.2 %in% t2]
       if(!is.nan(mean(element))){
-        vect <- c(mean(element, trim = 0.3), median(element), getmode(element), max(element), min(element), length(element)) 
-        print(vect)
-        lst[[type]] <- vect
+        nameVect <- c(nameVect, type)
+        meanVect <-  c(meanVect, mean(element, trim = 0.3))
+        medianVect <- c(medianVect, median(element))
+        modeVect <- c(modeVect, getmode(element))
+        maxVect <- c(maxVect, max(element))
+        minVect <- c(minVect, min(element))
+        size <- sum(size, length(element))
+        lengthVect <- c(lengthVect, size)
+        sizeVect <- c(sizeVect, length(element))
       }
     }
   }
-  return(lst)
+  return_df <- data.frame(nameVect, meanVect, medianVect, modeVect, maxVect, minVect, lengthVect, sizeVect)
+  return(return_df)
 }
 
 
@@ -50,4 +61,8 @@ types.1 <- getType(data$Type.1)
 types.2 <- getType(data$Type.2)
 number <- getSizeType(types.1, data)
 stats <- getStatsType(types.1, types.2, data)
-write.csv(stats, "output.csv", row.names = FALSE)
+orderByMean <- order(stats$meanVect)
+orderByLength <- order(stats$lengthVect)
+write.csv(stats[orderByMean,], "output-mean.csv", row.names = FALSE)
+write.csv(stats[orderByLength,], "output-length.csv", row.names = FALSE)
+write.csv(stats[order(stats$sizeVect),], "output-size.csv", row.names = FALSE)
