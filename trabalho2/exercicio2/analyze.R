2dc78570542cb349608a00dc3d8830a7f88baaba
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-10-02 15:29:26 -0300

    Finishing T2-Exercise 2;

--- trabalho2/exercicio2/analyze.R
+++ trabalho2/exercicio2/analyze.R
@@ -2,7 +2,19 @@ findPhi <- function(lines, columns) {
 return ((lines - 1) * (columns - 1))
 }
 
+findXn <- function (o, e) {
+  return (
+    (o - e) ** 2 / e
+  )
+}
 
+checkPValue <- function(pValue, alpha) {
+  if(pValue > alpha){
+    return("Don't reject H0")
+  } else {
+    return ("Reject H0")
+  }
+}
 
 sampleA <- read.csv("amostra_A_click.csv", header = TRUE, sep = ";")
 sampleB <- read.csv("amostra_B_click.csv", header = TRUE, sep = ";")
@@ -34,12 +46,17 @@ Z_ <- (G2 * CB) / T;
 #Teste Qui-Quadrado
 phi <- findPhi(2, 2);
 alpha <- 0.05
+observedVect <- c(X, W, Y, Z)
+expectedVect <- c(X_, W_, Y_, Z_)
+
+Xn <-  Reduce(sum, Map(findXn, observedVect, expectedVect))
+
+pValue <- pchisq(Xn, phi)
 
 #H0: amostras da mesma distribuicao
 #H1: amostras de distribuicao mesmo
 
+print(checkPValue(pValue, alpha));
 
-Print(X);
-print(W);
-print(Y);
-print(Z);
+
+
