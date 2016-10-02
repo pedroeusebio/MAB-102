9b9f7d36af43280a853bcaf2fed01c5620ff7e2a
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-10-02 14:47:46 -0300

    creating function to find Phi;

--- trabalho2/exercicio2/analyze.R
+++ trabalho2/exercicio2/analyze.R
@@ -1,3 +1,9 @@
+findPhi <- function(lines, columns) {
+return ((lines - 1) * (columns - 1))
+}
+
+
+
 sampleA <- read.csv("amostra_A_click.csv", header = TRUE, sep = ";")
 sampleB <- read.csv("amostra_B_click.csv", header = TRUE, sep = ";")
 
@@ -7,6 +13,7 @@ sampleAG2 <- sampleA$click_on[sampleA$click_on == "no"];
 sampleBG1 <- sampleB$click_on[sampleB$click_on == "yes"];
 sampleBG2 <- sampleB$click_on[sampleB$click_on == "no"];
 
+#Tabela Observada
 X <- length(sampleAG1);
 W <- length(sampleAG2);
 Y <- length(sampleBG1);
@@ -17,7 +24,22 @@ CA <- sum(X, W);
 CB <- sum(Y, Z);
 T <- sum(X, W, Y, Z);
 
-print(X);
+#Tabela Esperada
+
+X_ <- (G1 * CA) / T;
+W_ <- (G2 * CA) / T;
+Y_ <- (G1 * CB) / T;
+Z_ <- (G2 * CB) / T;
+
+#Teste Qui-Quadrado
+phi <- findPhi(2, 2);
+alpha <- 0.05
+
+#H0: amostras da mesma distribuicao
+#H1: amostras de distribuicao mesmo
+
+
+Print(X);
 print(W);
 print(Y);
 print(Z);
