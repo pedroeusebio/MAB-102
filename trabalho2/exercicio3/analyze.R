4946bd75cd21cc5b989cffdffb9aa84baed4347f
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-10-02 21:35:00 -0300

    Finishing exercise 3;

---
+++ trabalho2/exercicio3/analyze.R
@@ -0,0 +1,30 @@
+getAllNotNull <- function(v) {
+  return (!is.na(v))
+}
+
+getCurrelation <- function(imdb_score, data, dataNotNull) {
+  name <- names(data)
+  allCor <- c()
+  collName <- c()
+  for(row in 1:length(data)){
+    if(is.numeric(data[[row]][dataNotNull[[row]]][1]) & name[row] != 'imdb_score') {
+      collName <- c(collName, name[row])
+      allCor <- c(allCor, cor(imdb_score[dataNotNull[[row]]], data[[row]][dataNotNull[[row]]], use = "everything", method = "pearson"))
+    }
+  }
+  return( data.frame( "name" = collName, "correlation" = allCor))
+}
+
+# getting data
+data <- read.csv("movie_metadata.csv", header = TRUE, sep = ",")
+# getting all not null data)
+dataNotNull <- Map(getAllNotNull, data)
+# returning all correletion from all numeric columns
+result <- getCurrelation(data$imdb_score, data, dataNotNull)
+
+# get higher value
+sortedCor <- sort(result$correlation, index.return = TRUE, decreasing = TRUE)
+sortedName <- result$name[sortedCor$ix]
+
+ cat(" The higher correlation is ", sortedCor$x[1], ", the column is ", toString(sortedName[[1]]))
+
